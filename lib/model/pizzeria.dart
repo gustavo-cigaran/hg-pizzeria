71eab67f47bd31787908507ef6d448eb17507c4b
henrique macedo <henriquemacedo2016s@gmail.com>
2021-08-21 16:33:07 -0300

    segunda parte do banco de dados

---
+++ lib/model/pizzeria.dart
@@ -0,0 +1,30 @@
+import 'dart:js_util';
+
+class Pizzeria{
+    int? id;
+    String? flavor;
+    double? value;
+    String? description;
+
+    Pizzeria({this.id, this.flavor, this.value, this.description});
+
+    Pizzeria.fromMap(Map map){
+        id = map['id_column'] ?? 0;
+        flavor = map['falavor_column'];
+        value = map['value_column'];
+        description = map['description_column'];
+    }
+    Map<String, Object?> toMap(){
+        Map<String, Object?> map = {
+            "id_column": id,
+            "falavor_column": flavor,
+            "value_column": value,
+            "description_column": description,
+        };
+        if(id != null){
+            map['id_column'] = id;
+        }
+        return map;
+
+    }
+}
---
+++ lib/repository/pizzeria_repository.dart
@@ -0,0 +1,8 @@
+class PizzeriaRepository{
+
+  final String pizzeriaTable = "pizeria_table";
+  final String idColumn = "id_Column";
+  final String flavorColumn = "flavor_Column";
+  final String valueColumn = "value_Column";
+
+}
