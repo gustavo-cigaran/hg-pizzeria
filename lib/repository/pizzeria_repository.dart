0d0b46b62f421e9585553e28441ecc096359d816
henrique macedo <henriquemacedo2016s@gmail.com>
2021-08-21 17:29:37 -0300

    crud

--- lib/repository/pizzeria_repository.dart
+++ lib/repository/pizzeria_repository.dart
@@ -2,7 +2,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:projeto_final/model/model.dart';
 
-class PizzeriaRepository{
+class PizzeriaRepository {
 
   final String pizzeriaTable = "pizzeria_table";
   final String idColumn = "id_Column";
@@ -10,29 +10,71 @@ class PizzeriaRepository{
   final String valueColumn = "value_Column";
   final String descriptionColumn = "description_Column";
 
-  Future<Database> initializeDB() async{
+  Future<Database> initializeDB() async {
     final databasePath = await getDatabasesPath();
     final path = join(databasePath, "pizzeria.db");
 
     return openDatabase(path, version: 1,
       onCreate: (Database database, int newerVersion) async {
         await database.execute(
-          "CREATE TABLE $pizzeriaTable($idColumn INTEGER PRIMARY KEY, $flavorColumn TEXT, $valueColumn TEXT, $descriptionColumn TEXT"
+            "CREATE TABLE $pizzeriaTable($idColumn INTEGER PRIMARY KEY, $flavorColumn TEXT, $valueColumn TEXT, $descriptionColumn TEXT"
         );
       },
     );
   }
-  Future<int> save(Pizzeria pizzeria) async{
+
+  Future<int> save(Pizzeria pizzeria) async {
     final Database database = await initializeDB();
     return await database.insert(this.pizzeriaTable, pizzeria.toMap());
   }
 
-  Future<int> saveAll(List<Pizzeria> pizzeria) async{
-    int result= 0;
+  Future<int> saveAll(List<Pizzeria> pizzeria) async {
+    int result = 0;
     final Database database = await initializeDB();
-    for(var pizzeria in pizzeria){
+    for (var pizzeria in pizzeria) {
       result = await database.insert(this.pizzeriaTable, pizzeria.toMap());
     }
     return result;
   }
+
+  Future<List<Pizzeria>> fetchAll() async {
+    final Database database = await initializeDB();
+    final List<Map<String, Object?>> query = await database.query(
+        this.pizzeriaTable,
+        columns: [idColumn, flavorColumn, valueColumn, descriptionColumn]);
+    return query.map((element) => Pizzeria.fromMap(element)).toList();
+  }
+
+  // sipa nao vamos usar
+  Future<Pizzeria?> findById(int id) async {
+    final Database database = await initializeDB();
+    List<Map> maps = await database.query(
+        pizzeriaTable,
+        columns: [idColumn, flavorColumn, valueColumn, descriptionColumn],
+        where: "$idColumn = ?",
+        whereArgs: [id]
+    );
+
+    if (maps.length > 0)
+      return Pizzeria.fromMap(maps.first);
+    else
+      return null;
+  }
+
+  Future<int> delete(int id)async{
+    final Database database = await initializeDB();
+    return await database.delete(pizzeriaTable,where: "$idColumn = ?",
+    whereArgs: [id]
+    );
+  }
+  Future<int> uptade(Pizzeria pizzeria)async{
+    final Database database = await initializeDB();
+    return await database.update(
+        pizzeriaTable,
+        pizzeria.toMap(),
+        where: "$idColumn = ?",
+        whereArgs: [pizzeria.id]
+    );
+  }
+
 }
